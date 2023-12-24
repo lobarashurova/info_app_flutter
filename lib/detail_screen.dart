f5dfb7b6a33b1386d1464d58dfdb95afbfa911bb
lobarashurova <106048869+lobarashurova@users.noreply.github.com>
2023-12-24 23:15:46 +0500

    image

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -18,33 +18,59 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          backgroundColor: Color(0xFF031F2A),
-          body: SafeArea(
-            child: Column(
-            children: [
-              Container(
-                width: MediaQuery.sizeOf(context).width,
-                height: MediaQuery.sizeOf(context).height*0.45,
-                child: Stack(
-                  children: [
-                      Positioned(
-                        top: 0,
-                        bottom: 0,
-                        right: 0,
-                        left: 0,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.asset(
-                            regionData.regionImage,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-            ],
+      backgroundColor: Color(0xFF031F2A),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.sizeOf(context).width,
+            height: MediaQuery.sizeOf(context).height * 0.45,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
+                      regionData.regionImage,
+                      fit: BoxFit.cover,
                     ),
-          ));
+                  ),
+                ),
+                Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child:
+                    Image.asset('assets/visual.png')),
+                const Positioned(
+                    bottom: 25,
+                    left: 15,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Zomin",
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          "Tog'",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
